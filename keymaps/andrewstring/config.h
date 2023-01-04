8a09b9669db5a7cf4713d7c89b6dc93b50224bf0
Andrew Stringfellow <abreezemail@gmail.com>
2023-01-03 21:10:28 -0500

    updated

--- keymaps/andrewstring/config.h
+++ keymaps/andrewstring/config.h
@@ -17,8 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define MASTER_LEFT
-//#define MASTER_RIGHT
+//#define MASTER_LEFT
+#define MASTER_RIGHT
 #undef SOFT_SERIAL_PIN
 #define SOFT_SERIAL_PIN D2
 
--- keymaps/andrewstring/keymap.c
+++ keymaps/andrewstring/keymap.c
@@ -41,12 +41,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //                                               _______,_______,            _______,_______,
     //                                               _______,_______,            _______,_______
     // )
+
+    //BOTTOM 4:
+        //LEFT             RIGHT
+            //    1 3       3  1
+            //   2 4       4  2
+
     [_QWERTY] = LAYOUT_5x6(
         KC_EQUAL, KC_1, KC_2, KC_3, KC_4, KC_5,         KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BACKSLASH,
         KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G,         KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOTE,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,         KC_N, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_RSFT,
-                          KC_A, KC_B, KC_C,         KC_1, KC_2, KC_3,
-                    KC_G, KC_H, KC_I, KC_J,         KC_4, KC_5, KC_6, KC_7
+                  KC_LEFT, KC_RIGHT, KC_LCTL,         KC_RCTL, KC_UP, KC_DOWN,
+                    KC_BSPC, KC_ESCAPE, KC_I, KC_BTN1,         KC_SPACE, KC_LGUI, KC_ENTER, KC_BTN1
     )
 };
