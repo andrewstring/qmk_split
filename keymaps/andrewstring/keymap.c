0e551d428e05d7b4e0b3dfdc4ad93dd064ce2cf5
Andrew Stringfellow <abreezemail@gmail.com>
2023-01-03 21:24:21 -0500

    updated

--- keymaps/andrewstring/keymap.c
+++ keymaps/andrewstring/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
 #define _QWERTY 0
-#define _LOWER 1
+#define _EXTRA 1
 #define _RAISE 2
 
 #define RAISE MO(_RAISE)
@@ -49,6 +49,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT_5x6(
         KC_EQUAL, KC_1, KC_2, KC_3, KC_4, KC_5,         KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BACKSLASH,
+        KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G,         KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOTE,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,         KC_N, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_RSFT,
+                  KC_LEFT, KC_RIGHT, MO(1),         KC_RCTL, KC_UP, KC_DOWN,
+                    KC_BSPC, KC_ESCAPE, KC_DELETE, KC_BTN1,         KC_SPACE, KC_LGUI, KC_ENTER, KC_BTN1
+    ),
+
+    [_EXTRA] = LAYOUT_5x6(
+        KC_EQUAL, KC_1, KC_2, KC_3, KC_4, KC_5,         KC_I, KC_7, KC_8, KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, KC_MINUS,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BACKSLASH,
         KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G,         KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOTE,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,         KC_N, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_RSFT,
