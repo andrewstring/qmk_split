a4bf8e11de8fee93473da5aaa6d61575aebac513
Andrew Stringfellow <abreezemail@gmail.com>
2025-02-17 01:22:58 -0500

    updated gaming layer

--- NEW QMK/5x6/keymaps/andrewstring/keymap.c
+++ NEW QMK/5x6/keymaps/andrewstring/keymap.c
@@ -20,10 +20,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_EXTRA] = LAYOUT_5x6(
-        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,         TO(2), TO(3), KC_8, KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, KC_PRINT_SCREEN,
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,         LSFT(KC_LBRC), LSFT(KC_RBRC), KC_8, KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, KC_PRINT_SCREEN,
         KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,         KC_LBRC, KC_RBRC, KC_I, KC_O, KC_P, KC_BACKSLASH,
-        KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G,         LSFT(KC_9), LSFT(KC_0), KC_K, KC_L, KC_SCLN, KC_QUOTE,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,         KC_GRAVE, KC_TILDE, KC_COMM, KC_DOT, KC_SLASH, KC_RSFT,
+        KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G,         LSFT(KC_9), LSFT(KC_0), KC_K, KC_L, KC_SCLN, TO(3),
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,         KC_GRAVE, KC_TILDE, KC_COMM, KC_DOT, KC_SLASH, TO(2),
                 KC_LEFT, KC_RIGHT, KC_LCTL,               KC_RCTL, KC_UP, KC_DOWN,
            KC_BSPC, KC_ESCAPE, KC_I, KC_BTN1,          KC_SPACE, KC_LGUI, TO(2), KC_BTN1
     ),
