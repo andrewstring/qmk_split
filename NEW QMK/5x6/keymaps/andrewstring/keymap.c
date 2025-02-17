c487578ed2255143e6e2f963431fac060314999c
Andrew Stringfellow <abreezemail@gmail.com>
2025-02-17 00:09:06 -0500

    updated gaming layer

--- NEW QMK/5x6/keymaps/andrewstring/keymap.c
+++ NEW QMK/5x6/keymaps/andrewstring/keymap.c
@@ -28,12 +28,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_BSPC, KC_ESCAPE, KC_I, KC_BTN1,          KC_SPACE, KC_LGUI, TO(2), KC_BTN1
     ),
     [_GAMING] = LAYOUT_5x6(
-        TO(0), KC_A, KC_B, KC_C, KC_D, KC_E,         KC_E, KC_D, KC_C, KC_B, KC_A, KC_4,
-        KC_F, KC_G, KC_H, KC_UP, KC_J, KC_K,         KC_K, KC_J, KC_I, KC_H, KC_G, KC_F,
-        KC_L, KC_M, KC_LEFT, KC_DOWN, KC_RIGHT, KC_Q,         KC_Q, KC_P, KC_O, KC_N, KC_M, KC_L,
-        KC_R, KC_S, KC_T, KC_U, KC_V, KC_W,         KC_W, KC_V, KC_U, KC_T, KC_S, KC_R,
-                       KC_X, KC_Y, KC_Z,               KC_Z, KC_Y, KC_X,
-                  KC_SPACE, KC_ESCAPE, KC_ENTER, KC_3,            KC_3, KC_2, KC_1, KC_0
+        TO(0), KC_A, KC_B, KC_C, KC_D, KC_E,         KC_1, KC_2, KC_3, KC_4, KC_5, KC_TAB,
+        KC_TAB, KC_G, KC_H, KC_UP, KC_J, KC_K,         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BACKSLASH,
+        KC_L, KC_M, KC_LEFT, KC_DOWN, KC_RIGHT, KC_Q,         KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOTE,
+        KC_R, KC_S, KC_T, KC_U, KC_V, KC_W,         KC_N, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_RSFT,
+                       KC_X, KC_Y, KC_LCTL,               KC_RCTL, KC_UP, KC_DOWN,
+                  KC_SPACE, KC_ESCAPE, KC_ENTER, KC_3,            KC_SPACE, KC_GRAVE, KC_ENTER, KC_0
     ),
     [_PRODUCING] = LAYOUT_5x6(
         TO(0), KC_1, KC_2, KC_3, KC_4, KC_5,         KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS,
